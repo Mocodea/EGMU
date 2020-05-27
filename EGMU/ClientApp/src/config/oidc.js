2e5776b25ec1603c61e6e77d7728ca0c86a761e3
mrukas <markus.sussitz@gmail.com>
2020-05-27 02:30:33 +0200

    Changed Urls.

--- EGMU/ClientApp/src/config/oidc.js
+++ EGMU/ClientApp/src/config/oidc.js
@@ -1,9 +1,9 @@
 export const oidcSettings = {
-  authority: 'http://localhost:5000',
+  authority: 'https://egmu.mocodea.com',
   client_id: 'EGMU',
-  redirect_uri: 'http://localhost:5000/authentication/login-callback',
+  redirect_uri: 'https://egmu.mocodea.com/authentication/login-callback',
   response_type: 'code',
   scope: 'EGMUAPI openid profile',
-  silentRedirectUri: 'http://localhost:5000/authentication/silent-signin',
+  silentRedirectUri: 'https://egmu.mocodea.com/authentication/silent-signin',
   automaticSilentRenew: true, // If true oidc-client will try to renew your token when it is about to expire
 };
