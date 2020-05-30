570e2fb6465a56cbc39c195e0f1fd563648cefb7
mrukas <markus.sussitz@gmail.com>
2020-05-30 23:11:14 +0200

    Setup logging.

--- EGMU/Program.cs
+++ EGMU/Program.cs
@@ -1,20 +1,49 @@
 using EGMU.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Context;
+using Serilog.Events;
+using System;
+using System.IO;
 
 namespace EGMU
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
+            Log.Logger = new LoggerConfiguration()
+                      .ReadFrom.Configuration(Configuration)
+                      .CreateLogger();
 
-            ExecuteMigrations(host);
+            try
+            {
+                Log.Information($"Starting EGMU in {Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")} environment...");
 
-            host.Run();
+                var host = CreateHostBuilder(args).Build();
+                ExecuteMigrations(host);
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,14 +51,19 @@ namespace EGMU
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog();
 
         private static void ExecuteMigrations(IHost host)
         {
+            Log.Information("Starting Migrations...");
+
             using var scope = host.Services.CreateScope();
             var dbContext = scope.ServiceProvider.GetService<ApplicationDbContext>();
 
             dbContext.Database.Migrate();
+
+            Log.Information("Migrations finished.");
         }
     }
 }
